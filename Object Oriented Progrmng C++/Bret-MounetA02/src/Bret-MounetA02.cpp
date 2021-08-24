ed8e40999b55ca9d235dd828593f1498529d9200
rbretmounet <mrquacksxd@gmail.com>
2021-08-24 13:22:38 -0700

    First Assignment

---
+++ Object Oriented Progrmng C++/Bret-MounetA02/src/Bret-MounetA02.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Bret-MounetA02.cpp
+// Author      : Raphael Bret-Mounet
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "something other than !!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
