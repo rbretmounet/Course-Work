5600dee5f189cf0bc6cfe3d392c488c74de6089f
rbretmounet <mrquacksxd@gmail.com>
2021-09-15 10:30:36 -0700

    Cleaned up code and added comments.

--- Object Oriented Progrmng C++/Bret-MounetA1/src/Bret-MounetA1.cpp
+++ Object Oriented Progrmng C++/Bret-MounetA1/src/Bret-MounetA1.cpp
@@ -9,41 +9,50 @@
 #include <iostream>
 using namespace std;
 
+//Swaps two values
 void swap(int *x, int *y){
 	int temp = *x;
 	*x = *y;
 	*y = temp;
-
 }
 
+// Generates random numbers and populates array
 void random(int size, int *&array){
-	for (int i = 0; i < size; i++){
+	//Loops through each array index and assigns random number from 1-100
+	for (int i = 0; i < size; i++)
+	{
 		array[i] = rand() % 100 + 1;
 	}
 }
 
+// Creates new array and assigns to the array pointer
 void dynAlloc(int size, int * &array){
 	array = new int[size];
-	for (int i=0; i<size; i++) {
-	    array[i] = 0;
+}
+
+// Displays array
+void displayArray(int size, int *array){
+	for (int i = 0; i<size; i++)
+	{
+		cout << array[i] << endl;
 	}
 }
 
-void displayArray(int size, int *array){
-	for (int i = 0; i<size; i++){
-			cout << array[i] << endl;
-		}
-}
-
+// Sorts the array using bubble sort.
 int bubbleSort(int size, int *unsortedArray, int * sortedArray){
+	//Copies unsorted array to second array.
 	for (int i = 0; i < size; i++){
 		sortedArray[i] = unsortedArray[i];
 	}
+	//Intialize count
 	int count = 0;
-	for (int i = 0; i<size-1; i++){
-		for (int j = 0; j< size-i-1;j++)
+	
+	//Outside loop for 
+	for (int i = 0; i < size-1; i++){
+		for (int j = 0; j < size-i-1;j++)
 		{
-  			if (sortedArray[j] > sortedArray[j+1]){
+  			if (sortedArray[j] > sortedArray[j+1])
+			{
 				swap(&sortedArray[j], &sortedArray[j+1]);
 				count++;
 			}
@@ -52,6 +61,7 @@ int bubbleSort(int size, int *unsortedArray, int * sortedArray){
 	return count;
 }
 
+// Sorts the array using selection sort.
 int selectionSort(int size, int * unsortedArray, int * sortedArray){
 	for (int i = 0; i < size; i++){
 			sortedArray[i] = unsortedArray[i];
@@ -59,28 +69,34 @@ int selectionSort(int size, int * unsortedArray, int * sortedArray){
 		int count = 0;
 		for (int i = 0; i < size-1; i++)
 		    {
-		        int min_idx = i;
+		        int min = i;
 		        for (int j = i+1; j < size; j++)
-		        if (sortedArray[j] < sortedArray[min_idx]){
-		        	min_idx = j;
+		        if (sortedArray[j] < sortedArray[min])
+		        {
+		        	min = j;
 		        }
 
 
-		        swap(&sortedArray[min_idx], &sortedArray[i]);
+		        swap(&sortedArray[min], &sortedArray[i]);
 		        count++;
 		    }
 		return count;
 }
+
+//Searches the array for specified value using linear search.
 int linearSearch(int * array, int size, int value, int &count){
-	for (int i = 0; i < size; i++){
+	for (int i = 0; i < size; i++)
+	{
 		count++;
-		if (array[i] == value){
+		if (array[i] == value)
+		{
 			return i;
 		}
 	}
 	return -1;
 }
 
+//Searches the array using binary search.
 int binarySearch(int * array, int size, int value, int &count){
 	int left = 0;
 	int right = size-1;
@@ -88,11 +104,13 @@ int binarySearch(int * array, int size, int value, int &count){
 		int m = left + (right - left) / 2;
 		count++;
 
-	    if (array[m] == value){
+	    if (array[m] == value)
+	    {
 	        return m;
 	    }
 
-	    if (array[m] < value){
+	    if (array[m] < value)
+	    {
 	    	left = m + 1;
 	    }
 	    else
@@ -104,45 +122,66 @@ int binarySearch(int * array, int size, int value, int &count){
 }
 
 int main() {
+	//intialize variables
 	int *arrayPointer1 = NULL;
 	int *arrayPointer2 = NULL;
 	int input = 0;
-
+	
+	//Create arrays and assign to pointers.
 	dynAlloc(100,arrayPointer1);
 	dynAlloc(100,arrayPointer2);
-
+	
+	//Populates array with random numbers
 	random(100, arrayPointer1);
+
+	//Displays array values.
 	cout << "Unsorted Array:" << endl;
 	displayArray(100,arrayPointer1);
 
+	//Sorts array with bubble sort
 	int iterationCount = bubbleSort(100,arrayPointer1,arrayPointer2);
+
+	//Displays sorted array values
 	cout << "Sorted Array:" << endl;
 	displayArray(100,arrayPointer2);
 
 	cout << "Count: " << iterationCount << endl;
-
+	
+	//Sorts array with selection sort
 	iterationCount = selectionSort(100,arrayPointer1,arrayPointer2);
+
+	//Displays sorted array values
 	cout << "Sorted Array:" << endl;
 	displayArray(100,arrayPointer2);
-
+	
+	//Deletes content of pointer and points to null.
 	delete [] arrayPointer1;
 	arrayPointer1 = NULL;
 
 	cout << "Count: " << iterationCount << endl;
-
-	while (input != -1){
+	
+	//loop for searching sorted array.
+	while (input != -1)
+	{	
+		//Intialize variables
 		int count = 0;
 		int result = -1;
+		
+		//User prompt
 		cout << "Enter an Integer to Linearly Search For (-1 to Exit):";
 		cin >> input;
+
+		//Checks to see if user exits.
 		if (input == -1)
 		{
 			cout << "Program Terminated."<<endl;
 			break;
 		}
-
+		
+		//Returns results of search
 		result = linearSearch(arrayPointer2, 100,input,count);
-
+		
+		//Checks if integer was found or not
 		if (result == -1){
 			cout << "Integer not found!"<< endl;
 			cout << "Number of Iterations: " << count << endl;
@@ -151,18 +190,23 @@ int main() {
 			cout << "Integer found!"<< endl;
 			cout << "Number of Iterations: " << count << endl;
 		}
-
+		
+		//User prompt
 		cout << "Enter an Integer to Binary Search For (-1 to Exit):";
 		cin >> input;
+
+		//Checks if user trys to exit
 		if (input == -1)
 		{
 			cout << "Program Terminated."<<endl;
 			break;
 		}
 
+		//Resets count and gets result from binary search.
 		count = 0;
 		result = binarySearch(arrayPointer2, 100,input,count);
 
+		//Checks if interger was found or not
 		if (result == -1)
 		{
 			cout << "Integer not found!"<< endl;
@@ -174,5 +218,8 @@ int main() {
 		}
 	}
 
+	//Deletes content of pointer and points to null.
+	delete [] arrayPointer2;
+	arrayPointer2 = NULL;
 	return 0;
 }
